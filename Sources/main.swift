fe18f02252e92b0675c2dd34c46bd1a9cee07683
Boris Bügling <boris@icculus.org>
2015-12-21 16:39:39 +0100

    Implement `list` command

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,31 @@
 import Commander
 
+func getPackages(completion: [ATZPackage] -> ()) {
+  let downloader = ATZDownloader()
+  downloader.downloadPackageListWithCompletion() { packageList, _ in
+    let packages = ATZPackageFactory.createPackagesFromDicts(packageList)?.flatMap { $0 as? ATZPackage }
+    if let packages = packages {
+      completion(packages)
+    }
+  }
+}
+
+import AppKit
+
+func waitFor(block: () -> ()) {
+  NSApplicationLoad()
+  block()
+  NSApp.run()
+}
+
 Group {
   $0.command("list", description: "List installed plugins") {
-
+    waitFor() {
+      getPackages() { packages in
+        let packages = packages.filter { $0.isInstalled }.map { $0.name as String }
+        print(packages.joinWithSeparator("\n"))
+        exit(0)
+      }
+    }
   }
 }.run()
